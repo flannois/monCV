754cd2a0536a17e12a22c006840b18bd52362ebe
flannois <florent.lannois@gmail.com>
2021-02-08 18:01:36 +0100

    Création MVC

---
+++ monCV.py
@@ -0,0 +1,42 @@
+import cherrypy
+
+"""
+    def index(self):
+        page = self.creationPage("index")
+        return page
+    index.exposed = True
+"""
+
+class MonCV(object):
+    @cherrypy.expose
+
+    def rendu(self, page):
+        """ Va chercher le contenu du fichier {}.html
+            Renvoi le contenu de la page au format texte
+        """
+
+        page = 'pagesHTML/{}'.format(page)
+        with open(page,'r') as html:
+            page = html.readlines()
+        result = str()
+        for ligne in page:
+            result += ligne
+        return result
+
+    def creationPage(self, url):
+        """ Génére l'arborescence de toutes les pages
+            Renvoi la page completer (Haut + Menu + contenu etc.)
+        """
+        page = self.rendu("haut.html")
+        page += self.rendu("menu.html")
+        page += self.rendu("{}.html".format(url))
+        page += self.rendu("bas.html")
+        return page
+
+    def index(self):
+        page = self.creationPage("index")
+        return page
+    index.exposed = True
+
+    
+cherrypy.quickstart(MonCV(), config="monCV.conf")
